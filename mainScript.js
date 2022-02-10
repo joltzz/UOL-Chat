757ba668e75d66f60125cf7c115ce87d0a04cd48
joltzz <guijsilva02@gmail.com>
2022-02-10 14:22:29 -0300

    adicionado algumas funções de aplicação do nome

--- mainScript.js
+++ mainScript.js
@@ -1,19 +1,31 @@
     // Declarando variaveis
-let nome;
+let usuario={name:"guilherme"}
 let contato ="Todos";
-let visibilidade="";
+//tipos das mensagens
+let type = "message";
+let mensagensNaTela = document.querySelector(".mensagens ul");
+let participantesNaTela = document.querySelector(".participantes")
 
 
-    // Função de esconder o menu ao preencher o nome
-function entrar(){
-    const loginScreen=document.querySelector(".loginScreen");
-    nome=document.querySelector(".nome").value
-    //verificação para caso n tenha sido digitado nada
-    if(nome!==""){
-        loginScreen.classList.add("hide")
-    }else{
-        alert("Você precisa digitar seu nome primeiro!")
+    // Função de entrar
+function entrar () {
+    usuario.name = document.querySelector(".nome").value
+    if (usuario.name){
+        const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants",usuario )
+        promise.then(validarUsername).catch(erroLogin);
+        }
     }
+function validarUsername(resposta){
+    const telaLogin=document.querySelector(".loginScreen")
+    telaLogin.classList.add("hide")
+    carregarMensagens();
+    carregaParticipantes();
+    setInterval(recarregarPagina,3000);
+}
+function erroLogin(){
+    let nome=document.querySelector(".nome");
+    nome.value="";
+    document.querySelector(".loginScreen .mensagem p").innerHTML="Nome de usuário já está sendo utilizado!"
 }
 
     //Função para abrir o menu/barra lateral
